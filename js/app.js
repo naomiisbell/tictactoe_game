108aafffe1fc0fa3c3840bc83ffffddb707b760b
Naomi Isbell <naomiisbell@gmail.com>
2020-07-02 18:35:11 -0400

    tic tac toe arr val

--- js/app.js
+++ js/app.js
@@ -14,16 +14,12 @@ class PlayerTwo {
 
 let playerTwo = new PlayerTwo('O')
 
-const winningArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
+let ticTacToeArrVal = 0;
+
+const winningColArray = [
+    []
 ];
+
 
 
 // startOfGame()
@@ -40,12 +36,14 @@ let startOfGame = () => {
 let toggleVal = true;
 let toggle = (button) => {
     if(toggleVal === true) {
-        button.innerHTML = 'X'
+        button.innerHTML = 'X' //Unique symbols
         console.log("it's clicked")
+        ticTacToeArrVal = 1;
         toggleVal = false;
     }else if(toggleVal === false) {
         button.innerHTML = 'O'
         console.log("it's clicked")
+        ticTacToeArrVal = 0;
         toggleVal = true;
     }
 };
@@ -70,47 +68,47 @@ let clickOnBlock = (button) => {
 
 boardButton1.addEventListener('click', function(event){
     clickOnBlock(boardButton1) 
-    ticTacToe [0][0] = 1
+    ticTacToe [0][0] = ticTacToeArrVal
     console.log(ticTacToe)
 });
 boardButton2.addEventListener('click', function(event){
     clickOnBlock(boardButton2) 
-    ticTacToe [0][1] = 2
+    ticTacToe [0][1] = ticTacToeArrVal
     console.log(ticTacToe)
 });
 boardButton3.addEventListener('click', function(event){
     clickOnBlock(boardButton3)
-    ticTacToe [0][2] = 3
+    ticTacToe [0][2] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 boardButton4.addEventListener('click', function(event){
     clickOnBlock(boardButton4)
-    ticTacToe [1][0] = 4
+    ticTacToe [1][0] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 boardButton5.addEventListener('click', function(event){
     clickOnBlock(boardButton5)
-    ticTacToe [1][1] = 5
+    ticTacToe [1][1] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 boardButton6.addEventListener('click', function(event){
     clickOnBlock(boardButton6)
-    ticTacToe [1][2] = 6
+    ticTacToe [1][2] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 boardButton7.addEventListener('click', function(event){
     clickOnBlock(boardButton7)
-    ticTacToe [2][0] = 7
+    ticTacToe [2][0] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 boardButton8.addEventListener('click', function(event){
     clickOnBlock(boardButton8)
-    ticTacToe [2][1] = 8
+    ticTacToe [2][1] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 boardButton9.addEventListener('click', function(event){
     clickOnBlock(boardButton9)
-    ticTacToe [2][2] = 9
+    ticTacToe [2][2] = ticTacToeArrVal
     console.log(ticTacToe) 
 });
 
