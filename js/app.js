6601ab909b89126394215bde53287fd0e974be4e
Naomi Isbell <naomiisbell@gmail.com>
2020-07-02 17:07:01 -0400

    button js

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ const winningArray = [
 
 startOfGame()
 
-function startOfGame() {
+let startOfGame = () => {
     let playerTurn = playerTwo;
     if(playerTurn !== winningArray) {
         console.log('player ones turn')
@@ -44,23 +44,47 @@ function startOfGame() {
     }
 };
 
-let boardButton = document.querySelector('.btn-one')
-let boardButton = document.querySelector('.btn-two')
-let boardButton = document.querySelector('.btn-three')
-let boardButton = document.querySelector('.btn-four')
-let boardButton = document.querySelector('.btn-five')
-let boardButton = document.querySelector('.btn-six')
-let boardButton = document.querySelector('.btn-seven')
-let boardButton = document.querySelector('.btn-eight')
-let boardButton = document.querySelector('.btn-nine')
+let boardButton1 = document.querySelector('.btn-one')
+let boardButton2 = document.querySelector('.btn-two')
+let boardButton3 = document.querySelector('.btn-three')
+let boardButton4 = document.querySelector('.btn-four')
+let boardButton5 = document.querySelector('.btn-five')
+let boardButton6 = document.querySelector('.btn-six')
+let boardButton7 = document.querySelector('.btn-seven')
+let boardButton8 = document.querySelector('.btn-eight')
+let boardButton9 = document.querySelector('.btn-nine')
 
-function clickOnBlock() {
-    boardButton.innerHTML = 'X'
+let clickOnBlock = (button) => {
+    button.innerHTML = 'X'
     console.log("it's clicked")
 }
 
-boardButton.addEventListener('click', function(event){
-    clickOnBlock() 
+boardButton1.addEventListener('click', function(event){
+    clickOnBlock(boardButton1) 
+});
+boardButton2.addEventListener('click', function(event){
+    clickOnBlock(boardButton2) 
+});
+boardButton3.addEventListener('click', function(event){
+    clickOnBlock(boardButton3) 
+});
+boardButton4.addEventListener('click', function(event){
+    clickOnBlock(boardButton4) 
+});
+boardButton5.addEventListener('click', function(event){
+    clickOnBlock(boardButton5) 
+});
+boardButton6.addEventListener('click', function(event){
+    clickOnBlock(boardButton6) 
+});
+boardButton7.addEventListener('click', function(event){
+    clickOnBlock(boardButton7) 
+});
+boardButton8.addEventListener('click', function(event){
+    clickOnBlock(boardButton8) 
+});
+boardButton9.addEventListener('click', function(event){
+    clickOnBlock(boardButton9) 
 });
 
 
