e842deb9e5398026f0ace68bd9a044ff000e893b
Naomi Isbell <naomiisbell@gmail.com>
2020-07-03 02:25:59 -0400

    compArr Javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 class Player {
     constructor(symbol) {
         this.symbol = symbol
@@ -7,26 +9,51 @@ class Player {
 let playerOne = new Player("X");
 let playerTwo = new Player("O");
 
-let ticTacToeArrVal = 0;
+let ticTacToeArrVal = 1;
 
-const winningDiaArray = [
-    []
-];
+
 //quick comment
+
+//takes two arrays
+let compArr = (array_1, array_2) => {
+    if (array_1 === array_2) {
+        return true; //they are equal to each other
+    }
+    if (array_1.length !== array_2.length) {
+        return false; // different lengths
+    }
+    if (array_1 == null || array_2 == null) {
+        return false;
+    }
+    // comparing each array element 
+    // if it's not equal to the respective elements 
+    //looping through the elements
+    for (var i = 0; i <= array_1.length - 1; ++i) {
+        if (array_1[i] !== array_2[i]) {
+            return false;
+        }
+    }
+    return true; // if all statements are false the function will return true
+};
+
+let winValArr = [1, 1, 1]
+let winValArr_2 = [0, 0, 0]
 
 // SECTION
 let rowCheck = () => {
 
     //!player 1
     for (let i = 0; i < ticTacToe.length; i++) {
-        if (ticTacToe[i] == [1, 1, 1]) {
+        if (compArr(ticTacToe[i], winValArr)) {
             console.log("WIN")
+            alert("WIN")
         }
     }
     //!player 2
     for (let i = 0; i < ticTacToe.length; i++) {
-        if (ticTacToe[i] == [0, 0, 0]) {
+        if (compArr(ticTacToe[i], winValArr_2)) {
             console.log("WIN")
+            alert("WIN")
         }
     }
 };
@@ -36,16 +63,18 @@ let colCheck = () => {
 
     //!player 1
     for (let i = 0; i < ticTacToe.length; i++) {
-        testArr = [ticTacToe[0][i],ticTacToe[1][i],ticTacToe[2][i]]
-        if ( testArr[i] == [1, 1, 1]) {
+        testArr = [ticTacToe[0][i], ticTacToe[1][i], ticTacToe[2][i]]
+        if (compArr(testArr, winValArr)) {
             console.log("WIN")
+            alert("WIN")
+        }
     }
-}
     //!player 2
     for (let i = 0; i < ticTacToe.length; i++) {
-        testArr = [ticTacToe[0][i],ticTacToe[1][i],ticTacToe[2][i]]
-        if ( testArr[i] == [0, 0, 0]) {
+        testArr = [ticTacToe[0][i], ticTacToe[1][i], ticTacToe[2][i]]
+        if (compArr(testArr, winValArr_2)) {
             console.log("WIN")
+            alert("WIN")
         }
     }
 };
@@ -54,61 +83,58 @@ let colCheck = () => {
 let diagCheck = () => {
 
     //!left to right diagonal
-    if([ticTacToe[0][0],ticTacToe[1][1],ticTacToe[2][2]] == [1,1,1]) { 
+    if (compArr([ticTacToe[0][0], ticTacToe[1][1], ticTacToe[2][2]], winValArr)) {
         console.log("WIN")
+        alert("WIN")
 
     }
-        //!right to left diagonal
-    if([ticTacToe[2][0],ticTacToe[1][1],ticTacToe[0][2]] == [1,1,1]) { 
+    //!right to left diagonal
+    if (compArr([ticTacToe[2][0], ticTacToe[1][1], ticTacToe[0][2]], winValArr)) {
         console.log("WIN")
-
+        alert("WIN")
     }
     //!left to right diagonal
-    if([ticTacToe[0][0],ticTacToe[1][1],ticTacToe[2][2]] == [0,0,0]) { 
+    if (compArr([ticTacToe[0][0], ticTacToe[1][1], ticTacToe[2][2]], winValArr_2)) {
         console.log("WIN")
+        alert("WIN")
 
     }
-        //!right to left diagonal
-    if([ticTacToe[2][0],ticTacToe[1][1],ticTacToe[0][2]] == [0,0,0]) { 
+    //!right to left diagonal
+    if (compArr([ticTacToe[2][0], ticTacToe[1][1], ticTacToe[0][2]], winValArr_2)) {
         console.log("WIN")
-
+        alert("WIN")
     }
 }
 
-let startOfGame = () => {
-    let playerTurn = playerTwo;
-    if (playerTurn !== winningArray) {
-        console.log('player ones turn')
-    } else {
-        console.log('player won')
-    }
-};
 
-let toggleVal = true;
+
+let toggleVal = false;
 let toggle = (button) => {
 
     //! Player 1
-    if (toggleVal === true) {
+    if (toggleVal === false) {
         button.innerHTML = playerOne.symbol //* Unique symbols
         console.log("it's clicked")
         ticTacToeArrVal = 1;
-        toggleVal = false;
+        toggleVal = true;
 
         //! player 2        
-    } else if (toggleVal === false) {
-        button.innerHTML = playerTwo.symbol
+    } else if (toggleVal === true) {
+        //button.innerHTML = playerTwo.symbol
+        button.innerHTML = `<span class="player2">${playerTwo.symbol}</span>`
         console.log("it's clicked")
         ticTacToeArrVal = 0;
-        toggleVal = true;
+        toggleVal = false;
     }
 };
 
 const ticTacToe = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
+    [2, 2, 2],
+    [2, 2, 2],
+    [2, 2, 2]
+];
 
+// SECTION
 let boardButton1 = document.querySelector('.btn-one')
 let boardButton2 = document.querySelector('.btn-two')
 let boardButton3 = document.querySelector('.btn-three')
@@ -121,77 +147,86 @@ let boardButton9 = document.querySelector('.btn-nine')
 
 let clickOnBlock = (button) => {
     toggle(button)
+
 };
+let winChecks = () => {
+    rowCheck()
+    colCheck()
+    diagCheck()
+    boardCheck()
+}
 
 boardButton1.addEventListener('click', function (event) {
-    clickOnBlock(boardButton1)
-    ticTacToe[0][0] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[0][0] == 2) {
+        clickOnBlock(boardButton1)
+        ticTacToe[0][0] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
+
 });
 boardButton2.addEventListener('click', function (event) {
-    clickOnBlock(boardButton2)
-    ticTacToe[0][1] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[0][1] == 2) {
+        clickOnBlock(boardButton2)
+        ticTacToe[0][1] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton3.addEventListener('click', function (event) {
-    clickOnBlock(boardButton3)
-    ticTacToe[0][2] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[0][2] == 2) {
+        clickOnBlock(boardButton3)
+        ticTacToe[0][2] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton4.addEventListener('click', function (event) {
-    clickOnBlock(boardButton4)
-    ticTacToe[1][0] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[1][0] == 2) {
+        clickOnBlock(boardButton4)
+        ticTacToe[1][0] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton5.addEventListener('click', function (event) {
-    clickOnBlock(boardButton5)
-    ticTacToe[1][1] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[1][1] == 2) {
+        clickOnBlock(boardButton5)
+        ticTacToe[1][1] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton6.addEventListener('click', function (event) {
-    clickOnBlock(boardButton6)
-    ticTacToe[1][2] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[1][2] == 2) {
+        clickOnBlock(boardButton6)
+        ticTacToe[1][2] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton7.addEventListener('click', function (event) {
-    clickOnBlock(boardButton7)
-    ticTacToe[2][0] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[2][0] == 2) {
+        clickOnBlock(boardButton7)
+        ticTacToe[2][0] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton8.addEventListener('click', function (event) {
-    clickOnBlock(boardButton8)
-    ticTacToe[2][1] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[2][1] == 2) {
+        clickOnBlock(boardButton8)
+        ticTacToe[2][1] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
 boardButton9.addEventListener('click', function (event) {
-    clickOnBlock(boardButton9)
-    ticTacToe[2][2] = ticTacToeArrVal
-    console.log(ticTacToe)
-    rowCheck()
-    colCheck()
-    diagCheck()
+    if (ticTacToe[2][2] == 2) {
+        clickOnBlock(boardButton9)
+        ticTacToe[2][2] = ticTacToeArrVal
+        winChecks()
+        console.log(ticTacToe)
+    }
 });
+//- !SECTION
